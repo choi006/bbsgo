5fad45f4ec4dc79877a6f716faffc6ad4ac30404
choi <845461016@qq.com>
2022-04-25 13:55:01 +0800

    style: 代码格式更改

--- app/http/module/qa/api_question_delete.go
+++ app/http/module/qa/api_question_delete.go
@@ -1,13 +1,13 @@
 package qa
 
 import (
-    "github.com/choi006/bbsgo/app/http/middleware/auth"
-    provider "github.com/choi006/bbsgo/app/provider/qa"
-    "github.com/gohade/hade/framework/gin"
+	"github.com/choi006/bbsgo/app/http/middleware/auth"
+	provider "github.com/choi006/bbsgo/app/provider/qa"
+	"github.com/gohade/hade/framework/gin"
 )
 
 type questionDeleteParam struct {
-    ID int64 `json:"id" binding:"required"`
+	ID int64 `json:"id" binding:"required"`
 }
 
 // QuestionDelete 删除问题
@@ -21,34 +21,34 @@ type questionDeleteParam struct {
 // @Security ApiKeyAuth
 // @Router /question/delete [get]
 func (api *QaApi) QuestionDelete(c *gin.Context) {
-    // 参数校验
-    param := &questionDeleteParam{}
-    if err := c.ShouldBind(param); err != nil {
-        c.ISetStatus(400).IText("参数错误")
-        return
-    }
+	// 参数校验
+	param := &questionDeleteParam{}
+	if err := c.ShouldBind(param); err != nil {
+		c.ISetStatus(400).IText("参数错误")
+		return
+	}
 
-    qaService := c.MustMake(provider.QaKey).(provider.Service)
-    question, err := qaService.GetQuestion(c, param.ID)
-    if err != nil {
-        c.ISetStatus(500).IText(err.Error())
-        return
-    }
-    if question == nil {
-        c.ISetStatus(500).IText("问题不存在")
-        return
-    }
+	qaService := c.MustMake(provider.QaKey).(provider.Service)
+	question, err := qaService.GetQuestion(c, param.ID)
+	if err != nil {
+		c.ISetStatus(500).IText(err.Error())
+		return
+	}
+	if question == nil {
+		c.ISetStatus(500).IText("问题不存在")
+		return
+	}
 
-    user := auth.GetAuthUser(c)
-    if user.ID != question.AuthorID {
-        c.ISetStatus(500).IText("无权限操作")
-        return
-    }
+	user := auth.GetAuthUser(c)
+	if user.ID != question.AuthorID {
+		c.ISetStatus(500).IText("无权限操作")
+		return
+	}
 
-    if err := qaService.DeleteQuestion(c, question.ID); err != nil {
-        c.ISetStatus(500).IText(err.Error())
-        return
-    }
+	if err := qaService.DeleteQuestion(c, question.ID); err != nil {
+		c.ISetStatus(500).IText(err.Error())
+		return
+	}
 
-    c.ISetOkStatus().IText("操作成功")
+	c.ISetOkStatus().IText("操作成功")
 }
